b9cd6b9856e5019b83c9846089cb81ff08afa448
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2020-12-10 13:32:06 +0000

    Test to check that input is whole number

--- tests/primeNumbers.test.js
+++ tests/primeNumbers.test.js
@@ -7,14 +7,16 @@ const primesMultiplication = require('../index')
     // should be a whole number
 describe("validate user input N", () => {
     test("should be of type Number", () => {
-        expect(primesMultiplication(2)).toBe("number");
         expect(primesMultiplication("string")).toBe("Please insert a number");
     });
 
     test("user input should be atleast 1", () => {
         expect(primesMultiplication(-1)).toBe("Please insert a value of at least 1");
     });
+    test("check number is a whole number", () => {
+        expect(primesMultiplication(1.2).toBe("Please insert a whole number"));
 
+    })
 
 });
 
