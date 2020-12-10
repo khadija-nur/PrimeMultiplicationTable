77f24fc5bf5ef80547fe053bf36108c63eaf1dce
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2020-12-10 13:35:01 +0000

    correct typo: missing bracket

--- tests/primeNumbers.test.js
+++ tests/primeNumbers.test.js
@@ -14,7 +14,7 @@ describe("validate user input N", () => {
         expect(primesMultiplication(-1)).toBe("Please insert a value of at least 1");
     });
     test("check number is a whole number", () => {
-        expect(primesMultiplication(1.2).toBe("Please insert a whole number"));
+        expect(primesMultiplication(1.2)).toBe("Please insert a whole number");
 
     })
 
