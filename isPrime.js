3f7641bdf5157e07235f422263058b088a1e4711
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2020-12-10 15:26:49 +0000

    export function individually

--- isPrime.js
+++ isPrime.js
@@ -1,7 +1,7 @@
-const isPrime = num => {
+module.exports = (num) =>{
     for(let i = 2; i < num; i++)
       if(num % i === 0) return false;
-    return num > 1;
+    return num  > 1;
   }
 
-module.exports = {isPrime};
+ 
