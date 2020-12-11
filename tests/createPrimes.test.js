4c6b9c8ae04008730277eb0b365ae4887e7e8c9a
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2020-12-11 11:33:53 +0000

    Add more tests to check generated prime numbers

--- tests/createPrimes.test.js
+++ tests/createPrimes.test.js
@@ -4,13 +4,16 @@ const isPrime = require("../helpers/isPrime")
 
     test("only N amount of numbers should be generated", () => {
         expect(primesMultiplication(5).length).toBe(5);
+        expect(primesMultiplication(11).length).toBe(24;
+        expect(primesMultiplication(24).length).toBe(11);
+
     });
 
     test("the first number should be 2", () => {
         expect(primesMultiplication(5)[0]).toBe(2);
     });
 
-    test("the numbers returned should be prime", () => {
+    test("the first number returned in the array should be prime", () => {
         expect(isPrime(primesMultiplication(5)[0])).toBe(true);
     });
 
