e5498e99bafa715c01ec94fbf7c8c2f0263216d1
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2020-12-13 17:36:19 +0000

    format file

--- helpers/displayTable.js
+++ helpers/displayTable.js
@@ -14,43 +14,39 @@ use a function that create rows for the table :
 
 */
 
-
 export default (arrayStack) => {
-    // get the reference for the body
-    var tblContainer = document.querySelector("main");
-  
-    // creates a <table> element and a <tbody> element
-    var tbl = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-  
-    // creating all cells
-    arrayStack.forEach(row => {
-      var tableRow = document.createElement("tr");
-      row.forEach(column => {
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode( column);
-        cell.appendChild(cellText);
-        tableRow.appendChild(cell);
-      });
-      tblBody.appendChild(tableRow);
+	// get the reference for the body
+	var tblContainer = document.querySelector('main');
 
-      
-    }); 
-      // creates a table row
-  
-  
-      // add the row to the end of the table body
-  
-    // put the <tbody> in the <table>
-    tbl.appendChild(tblBody);
-    // appends <table> into <body>
-    console.log(tbl)
-    console.log(tblContainer)
-    tblContainer.innerHTML = "";
-    tblContainer.appendChild(tbl)
-    // sets the border attribute of tbl to 2;
-    tbl.setAttribute("border", "2");
-  }
+	// creates a <table> element and a <tbody> element
+	var tbl = document.createElement('table');
+	var tblBody = document.createElement('tbody');
+
+	// creating all cells
+	arrayStack.forEach((row) => {
+		var tableRow = document.createElement('tr');
+		row.forEach((column) => {
+			// Create a <td> element and a text node, make the text
+			// node the contents of the <td>, and put the <td> at
+			// the end of the table row
+			var cell = document.createElement('td');
+			var cellText = document.createTextNode(column);
+			cell.appendChild(cellText);
+			tableRow.appendChild(cell);
+		});
+		tblBody.appendChild(tableRow);
+	});
+	// creates a table row
+
+	// add the row to the end of the table body
+
+	// put the <tbody> in the <table>
+	tbl.appendChild(tblBody);
+	// appends <table> into <body>
+	console.log(tbl);
+	console.log(tblContainer);
+	tblContainer.innerHTML = '';
+	tblContainer.appendChild(tbl);
+	// sets the border attribute of tbl to 2;
+	tbl.setAttribute('border', '2');
+};
