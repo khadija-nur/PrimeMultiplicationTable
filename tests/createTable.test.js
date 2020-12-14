4053e6c21d43ea13bee6d9b5e6bd6891bd297617
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2020-12-14 01:27:37 +0000

    Add more tests for other edge cases

--- tests/createTable.test.js
+++ tests/createTable.test.js
@@ -3,15 +3,24 @@ import createTable from '../helpers/createMultiplicationTable';
 // should be a number
 // should be at least 1
 // should be a whole number
-describe('', () => {
+describe('confirm output type is accurate', () => {
 	test('Table should be an array', () => {
 		expect(typeof createTable([2, 3])).toBe('object');
 	});
-
 	test('Table should have at least one array inside', () => {
 		expect(createTable([2]).length).toBeGreaterThan(0);
 	});
+});
+describe('check if function returns the correct output ', () => {
 	test('Number of arrays within the table should be one more than the number of prime number in the array it receives', () => {
 		expect(createTable([2, 3, 5, 7]).length).toBe(5);
+		expect(createTable([2, 3, 5, 7, 11]).length).toBe(6);
+		expect(createTable([2, 3, 5, 7, 11, 13]).length).toBe(7);
+		expect(createTable([2, 3, 5, 7, 11, 13, 17]).length).toBe(8);
+	});
+	test('empty array of prime numbers input should return an error message', () => {
+		expect(createTable([])).toBe(
+			'table cannot be generated if there are no primary numbers provided'
+		);
 	});
 });
